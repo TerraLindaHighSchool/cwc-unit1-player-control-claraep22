2a9c26786c1b5edf954d53ace95ae26074a515a6
claraep22 <ce00966@srcs.org>
2020-01-16 15:20:58 -0800

    Tutorial 1.4.2 Complete

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
     public float speed = 5.0f;
+    public float turnSpeed;
 
     void Start()
     {
@@ -17,5 +18,6 @@ public class PlayerController : MonoBehaviour
     {
         // Move the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
     }
 }
